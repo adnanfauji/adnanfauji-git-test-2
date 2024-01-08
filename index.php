5e765257bf7ea4e9d73250f69d653ba85f9395ed
adnanfauji <adnanfauji48@gmail.com>
2024-01-08 11:46:09 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, odit. Impedit quos nihil cupiditate, reprehenderit a eum molestiae magnam eaque nam quasi alias vel animi, debitis excepturi distinctio totam minima.</p>
 </body>
 </html>
